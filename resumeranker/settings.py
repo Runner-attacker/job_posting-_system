016d362d085667fe15ed4467c12414da2a1ae8eb
Sahil-Bhisma <bhisma.koirala@bidhee.com>
2025-08-02 18:10:05 +0545

    Set  custom user model, registration and JWT authentication

--- resumeranker/settings.py
+++ resumeranker/settings.py
@@ -108,6 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+AUTH_USER_MODEL = 'users.CustomUser'
 
 
 # Internationalization
--- users/models.py
+++ users/models.py
@@ -1,3 +1,41 @@
+from django.contrib.auth.models import AbstractBaseUser , BaseUserManager,PermissionsMixin
 from django.db import models
+from django.utils import timezone
+
 
 # Create your models here.
+
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self , email , password=None ,**extra_fields):
+        if not email:
+            raise ValueError("Users must have an email address")
+        email = self.normalize_email(email)
+        user = self.model(email=email)
+        user.set_password(password)
+        user.save()
+        return user
+    
+    def create_superuser(self , email , password=None , **extra_fields):
+        extra_fields.setdefault("is_staff",True)
+        extra_fields.setdefault("is_superuser",True)
+        return self.create_user(email , password , **extra_fields)
+
+class CustomUser(AbstractBaseUser,PermissionsMixin):
+    email = models.EmailField(unique=True)
+    name = models.CharField(max_length=100)
+    is_employer = models.BooleanField(default=False)
+    is_jobseeker = models.BooleanField(default=False)
+
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    date_joined = models.DateTimeField(default=timezone.now)
+
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name']
+
+    def __str__(self):
+        return self.email
